8d1a59c3c6d7a3301e8fef66f5f0eb381e6efe45
Sergey Bortsov <jonnyyboss98@gmail.com>
2022-10-19 13:45:42 +0300

    v1.54

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express()
 const PORT = process.env.PORT ?? 3000
 
 app.get("/", function(request, response){
-    response.render('index.html')
+    response.render('index')
 })
 app.listen(PORT, () => {
     console.log(`Server has been started on port ${PORT}...`)
